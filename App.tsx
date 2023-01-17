07bbec0de3ffed3571952b9ad48dc1edf0eacd0c
zake-dev <floatim00@gmail.com>
2023-01-17 20:52:19 +0900

    feat: add fcm device token save hook, fcm foreground listener hook, fcm background listener.
    Added hooks for handling firebase cloud messaging push notifications.

--- App.tsx
+++ App.tsx
@@ -1,12 +1,20 @@
 import React from "react";
 
+import {FirebaseMessagingTypes} from "@react-native-firebase/messaging";
+import {Alert} from "react-native";
 import {SafeAreaProvider} from "react-native-safe-area-context";
 import {Provider} from "react-redux";
 
+import {useFcmForegroundMessageListener, useFcmTokenSave} from "@/hooks/PushNotifications";
 import Navigation from "@/navigation";
 import {store} from "@/store";
 
 export default function App() {
+  useFcmTokenSave();
+  useFcmForegroundMessageListener(async (remoteMessage: FirebaseMessagingTypes.RemoteMessage) => {
+    Alert.alert(JSON.stringify(remoteMessage));
+  });
+
   return (
     <Provider store={store}>
       <SafeAreaProvider>
