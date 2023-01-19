57476b36cbdcd7b3c41b5ae9103c72f5e33a9632
zake-dev <floatim00@gmail.com>
2023-01-19 13:25:22 +0900

    feat: add design system constants (typography, colors)

--- src/constants/colors.ts
+++ src/constants/colors.ts
@@ -2,6 +2,29 @@ const colors = {
   warning: "#FF6B6B",
   placeholder: "#4B5563",
   background: "#111B26",
+
+  white: "#FFFFFF",
+
+  OTBBlack: "#111B26",
+  OTBBlack800: "#1F2937",
+  OTBBlack700: "#374151",
+  OTBBlack600: "#4B5563",
+  OTBBlack500: "#6B7280",
+  OTBBlack400: "#9CA3AF",
+  OTBBlack300: "#CBD5E1",
+  OTBBlack200: "#E2E8F0",
+  OTBBlack100: "#F1F5F9",
+  OTBBlack50: "#F8FAFC",
+
+  OTBBlueDark: "#0F0AD7",
+  OTBBlue: "#3732FF",
+  OTBBlueLight1: "#4B46FF",
+  OTBBlueLight2: "#5F5AFF",
+  OTBBlueLight3: "#736EFF",
+  OTBBlueLight4: "#8782FF",
+  OTBBlueLight5: "#9B96FF",
+
+  noticeRed: "#FF6B6B",
 };
 
 export default colors;
---
+++ src/constants/typography.ts
@@ -0,0 +1,118 @@
+import {StyleSheet} from "react-native";
+
+const typography = StyleSheet.create({
+  display01: {
+    fontFamily: "PretendardVariable",
+    fontSize: 40,
+    fontWeight: "700",
+    lineHeight: 52,
+    letterSpacing: -0.6,
+  },
+  display02: {
+    fontFamily: "PretendardVariable",
+    fontSize: 36,
+    fontWeight: "700",
+    lineHeight: 46,
+    letterSpacing: -0.6,
+  },
+  display03: {
+    fontFamily: "PretendardVariable",
+    fontSize: 32,
+    fontWeight: "700",
+    lineHeight: 42,
+    letterSpacing: -0.6,
+  },
+  display04: {
+    fontFamily: "PretendardVariable",
+    fontSize: 28,
+    fontWeight: "700",
+    lineHeight: 38,
+    letterSpacing: -0.6,
+  },
+  display05: {
+    fontFamily: "PretendardVariable",
+    fontSize: 24,
+    fontWeight: "700",
+    lineHeight: 34,
+    letterSpacing: -0.6,
+  },
+  headline: {
+    fontFamily: "PretendardVariable",
+    fontSize: 20,
+    fontWeight: "700",
+    lineHeight: 28,
+    letterSpacing: -0.6,
+  },
+  subhead01: {
+    fontFamily: "PretendardVariable",
+    fontSize: 16,
+    fontWeight: "700",
+    lineHeight: 22,
+    letterSpacing: -0.6,
+  },
+  subheadLong01: {
+    fontFamily: "PretendardVariable",
+    fontSize: 16,
+    fontWeight: "700",
+    lineHeight: 28,
+    letterSpacing: -0.6,
+  },
+  subhead02: {
+    fontFamily: "PretendardVariable",
+    fontSize: 14,
+    fontWeight: "700",
+    lineHeight: 20,
+    letterSpacing: -0.6,
+  },
+  subheadLong02: {
+    fontFamily: "PretendardVariable",
+    fontSize: 14,
+    fontWeight: "700",
+    lineHeight: 24,
+    letterSpacing: -0.6,
+  },
+  subhead03: {
+    fontFamily: "PretendardVariable",
+    fontSize: 12,
+    fontWeight: "700",
+    lineHeight: 18,
+    letterSpacing: -0.6,
+  },
+  body01: {
+    fontFamily: "PretendardVariable",
+    fontSize: 16,
+    fontWeight: "400",
+    lineHeight: 24,
+    letterSpacing: -0.6,
+  },
+  bodyLong01: {
+    fontFamily: "PretendardVariable",
+    fontSize: 16,
+    fontWeight: "400",
+    lineHeight: 28,
+    letterSpacing: -0.6,
+  },
+  body02: {
+    fontFamily: "PretendardVariable",
+    fontSize: 14,
+    fontWeight: "400",
+    lineHeight: 20,
+    letterSpacing: -0.6,
+  },
+  bodyLong02: {
+    fontFamily: "PretendardVariable",
+    fontSize: 14,
+    fontWeight: "400",
+    lineHeight: 24,
+    letterSpacing: -0.6,
+  },
+  caption: {
+    fontFamily: "PretendardVariable",
+    fontSize: 12,
+    fontWeight: "400",
+    lineHeight: 18,
+    letterSpacing: -0.6,
+  },
+});
+
+export default typography;
