9d99bc74970eb4f9cd58089e6cd3fcf09bbe692f
zake-dev <floatim00@gmail.com>
2023-01-29 07:38:51 +0900

    style: apply code convention

--- src/hooks/PushNotifications/src/useFcmTokenSave.ts
+++ src/hooks/PushNotifications/src/useFcmTokenSave.ts
@@ -1,8 +1,8 @@
-import React, {useEffect} from "react";
+import {useEffect} from "react";
 
 import messaging from "@react-native-firebase/messaging";
 
-function useFcmTokenSave() {
+export default function useFcmTokenSave() {
   async function saveTokenToDatabase(token: string) {
     // TODO: 사용자 로그인 인증 확인
     // TODO: 실제 DB에 저장하는 로직 추가
@@ -23,5 +23,3 @@ function useFcmTokenSave() {
     });
   }, []);
 }
-
-export default useFcmTokenSave;
