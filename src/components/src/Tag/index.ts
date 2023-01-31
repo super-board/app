c063c453e90f1e8a667312ea6d478aea84a69cb0
zake-dev <floatim00@gmail.com>
2023-01-31 10:21:00 +0900

    feat: add selected tag list on Recommendation screen

--- src/components/src/Tag/index.ts
+++ src/components/src/Tag/index.ts
@@ -1,1 +1,2 @@
+export {default as SelectedTagsHorizontalListView} from "./src/SelectedTagsHorizontalListView";
 export {default as TagChip} from "./src/TagChip";
--- src/screens/search/src/RecommendationScreen.tsx
+++ src/screens/search/src/RecommendationScreen.tsx
@@ -2,10 +2,15 @@ import React from "react";
 
 import {StyleSheet, View} from "react-native";
 
+import {SelectedTagsHorizontalListView} from "@/components";
 import colors from "@/constants/colors";
 
 export default function RecommendationScreen() {
-  return <View style={styles.container}></View>;
+  return (
+    <View style={styles.container}>
+      <SelectedTagsHorizontalListView />
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
