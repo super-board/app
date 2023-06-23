78b1d60d1637042d6285dc62e25418c6f89fbadd
zake-dev <floatim00@gmail.com>
2023-06-23 10:54:36 +1000

    chore: update profile image assets

--- src/assets/svgs/index.ts
+++ src/assets/svgs/index.ts
@@ -2,6 +2,5 @@ import * as Badge from "./src/badge";
 import * as Common from "./src/common";
 import * as Icon from "./src/icon";
 import * as Logo from "./src/logo";
-import * as Profile from "./src/profile";
 
-export const SVG = {Common, Icon, Profile, Badge, Logo};
+export const SVG = {Common, Icon, Badge, Logo};
--- src/components/src/ProfileImage.tsx
+++ src/components/src/ProfileImage.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 
-import {SVG} from "@/assets/svgs";
+import FastImage from "react-native-fast-image";
+
 import {ProfileCharacter} from "@/types";
 
 type Props = {
@@ -12,22 +13,67 @@ type Props = {
 export default function ProfileImage({type, width = 80, height = 80}: Props) {
   switch (type) {
     case "PROFILE_1":
-      return <SVG.Profile.Profile1 width={width} height={height} />;
+      return (
+        <FastImage
+          source={require("@/assets/images/profile/profile1.png")}
+          style={{width, height}}
+        />
+      );
     case "PROFILE_2":
-      return <SVG.Profile.Profile2 width={width} height={height} />;
+      return (
+        <FastImage
+          source={require("@/assets/images/profile/profile2.png")}
+          style={{width, height}}
+        />
+      );
     case "PROFILE_3":
-      return <SVG.Profile.Profile3 width={width} height={height} />;
+      return (
+        <FastImage
+          source={require("@/assets/images/profile/profile3.png")}
+          style={{width, height}}
+        />
+      );
     case "PROFILE_4":
-      return <SVG.Profile.Profile4 width={width} height={height} />;
+      return (
+        <FastImage
+          source={require("@/assets/images/profile/profile4.png")}
+          style={{width, height}}
+        />
+      );
     case "PROFILE_5":
-      return <SVG.Profile.Profile5 width={width} height={height} />;
+      return (
+        <FastImage
+          source={require("@/assets/images/profile/profile5.png")}
+          style={{width, height}}
+        />
+      );
     case "PROFILE_6":
-      return <SVG.Profile.Profile6 width={width} height={height} />;
+      return (
+        <FastImage
+          source={require("@/assets/images/profile/profile6.png")}
+          style={{width, height}}
+        />
+      );
     case "PROFILE_7":
-      return <SVG.Profile.Profile7 width={width} height={height} />;
+      return (
+        <FastImage
+          source={require("@/assets/images/profile/profile7.png")}
+          style={{width, height}}
+        />
+      );
     case "PROFILE_8":
-      return <SVG.Profile.Profile8 width={width} height={height} />;
+      return (
+        <FastImage
+          source={require("@/assets/images/profile/profile8.png")}
+          style={{width, height}}
+        />
+      );
     case "PROFILE_9":
-      return <SVG.Profile.Profile9 width={width} height={height} />;
+      return (
+        <FastImage
+          source={require("@/assets/images/profile/profile9.png")}
+          style={{width, height}}
+        />
+      );
   }
 }
